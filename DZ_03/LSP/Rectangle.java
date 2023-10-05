13d9571d85e33e0abf00a47dba56d68a6287feee
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2023-10-05 23:41:06 +0700

    Create Rectangle.java

---
+++ DZ_03/LSP/Rectangle.java
@@ -0,0 +1,19 @@
+package LSP;
+
+public class Rectangle implements iShape {
+    private int width;
+    private int height;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    @Override
+    public int area() {
+        return width * height;
+    }
+}
