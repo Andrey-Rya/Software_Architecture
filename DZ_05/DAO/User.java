682326688e08a603a5341138642316e34ce0a27c
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2023-10-15 12:39:49 +0700

    Create User.java

---
+++ DZ_05/DAO/User.java
@@ -0,0 +1,15 @@
+package DAO;
+
+// Класс модели данных
+public class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+    }
+
+    public void setAge(int i) {
+    }
+
+    // Геттеры и сеттеры
+}
