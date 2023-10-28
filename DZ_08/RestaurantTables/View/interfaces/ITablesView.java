bebf7a16ee5fe34c66ae21fdba8881b5c6f1eaf6
Andrey-R <109613667+Andrey-Rya@users.noreply.github.com>
2023-10-28 12:14:02 +0700

    Add files via upload

--- DZ_08/RestaurantTables/View/interfaces/ITablesView.java
+++ DZ_08/RestaurantTables/View/interfaces/ITablesView.java
@@ -1,1 +1,15 @@
-1
+package RestaurantTables.View.interfaces;
+
+public interface ITablesView {
+    void showTablesStatus(String[][] tablesStatus);
+
+    void showSuccessMessage(String message);
+
+    void showErrorMessage(String message);
+
+    int selectHall();
+
+    int selectTable();
+
+    int selectOption();
+}
